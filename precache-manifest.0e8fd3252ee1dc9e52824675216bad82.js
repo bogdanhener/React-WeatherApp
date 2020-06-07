428ae7170d5cff4b06f4bee266f921e7cd8417c5
Bogdan Hener <bogdan@heners94.com>
2020-06-07 15:30:02 +0300

    Updates

---
+++ precache-manifest.0e8fd3252ee1dc9e52824675216bad82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342b81a9e528675c15dbfa07a7f44603",
+    "url": "/React-WeatherApp/index.html"
+  },
+  {
+    "revision": "d659a1e529e20902ae80",
+    "url": "/React-WeatherApp/static/css/main.87b83c48.chunk.css"
+  },
+  {
+    "revision": "ee2adf8542f28a9f3e01",
+    "url": "/React-WeatherApp/static/js/2.373c2334.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-WeatherApp/static/js/2.373c2334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d659a1e529e20902ae80",
+    "url": "/React-WeatherApp/static/js/main.13b692fd.chunk.js"
+  },
+  {
+    "revision": "da38c8823d20913348a8",
+    "url": "/React-WeatherApp/static/js/runtime-main.1ca2391a.js"
+  },
+  {
+    "revision": "245af6092b99c13b57d47926450e13c9",
+    "url": "/React-WeatherApp/static/media/main-app.245af609.jpg"
+  },
+  {
+    "revision": "f7fa4981414626c52525516806f26ab3",
+    "url": "/React-WeatherApp/static/media/spring.f7fa4981.jpg"
+  },
+  {
+    "revision": "f8a324c0580a9b91334f0122fffe11f0",
+    "url": "/React-WeatherApp/static/media/summer.f8a324c0.jpg"
+  },
+  {
+    "revision": "9188aefade34d8b3202520261514cc03",
+    "url": "/React-WeatherApp/static/media/winter.9188aefa.jpg"
+  }
+]);
